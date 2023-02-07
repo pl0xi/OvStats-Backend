e408d1d4b6906c631b2fd6cf42f9bf1c1173d0a7
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-02-07 13:44:44 +0100

    Removed examples from template

---
+++ OvStats_Website/OvStats_Website/Controllers/LeagueController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OvStats_Website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeagueController : ControllerBase
+    {
+    }
+}
