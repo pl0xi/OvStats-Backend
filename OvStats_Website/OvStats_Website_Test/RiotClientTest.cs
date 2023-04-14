000cac1da4a73a6c3f189fc4436ffbc1c564a5c1
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-04-14 18:16:52 +0200

    GetAccount function test

--- OvStats_Website/OvStats_Website_Test/RiotClientTest.cs
+++ OvStats_Website/OvStats_Website_Test/RiotClientTest.cs
@@ -1,11 +1,28 @@
+using Moq;
+using OvStats_Website.Clients;
+using OvStats_Website.DTO;
+
 namespace OvStats_Website_Test
 {
     public class RiotClientTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly IRiotClient _IRiotClient;
 
+        public RiotClientTest()
+        {
+            Mock<HttpClient> mockHttpClient = new Mock<HttpClient>();
+            _IRiotClient = new RiotClient(mockHttpClient.Object);
+        }
+
+        [Fact]
+        public async Task GetAccountTestAsync()
+        {
+            string username = "sofieee";
+            string region = "euw1";
+            SummonerAccountDTO summonerAccountDTO = await _IRiotClient.GetAccount(username, region);
+
+            Assert.NotNull(summonerAccountDTO.id);
+            Assert.Equal("sofieee", summonerAccountDTO.name);
         }
     }
 }
