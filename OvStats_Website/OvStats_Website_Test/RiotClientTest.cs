c257430e5b00a6c83d5590549c7e53b27369f684
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-04-15 00:27:55 +0200

    GetSummonerInfo function test

--- OvStats_Website/OvStats_Website_Test/RiotClientTest.cs
+++ OvStats_Website/OvStats_Website_Test/RiotClientTest.cs
@@ -1,21 +1,28 @@
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Moq;
 using OvStats_Website.Clients;
 using OvStats_Website.DTO;
+using Xunit.Abstractions;
 
 namespace OvStats_Website_Test
 {
     public class RiotClientTest
     {
         private readonly IRiotClient _IRiotClient;
+        private readonly ITestOutputHelper _output;
 
-        public RiotClientTest()
+
+        public RiotClientTest(ITestOutputHelper output)
         {
             Mock<HttpClient> mockHttpClient = new Mock<HttpClient>();
             _IRiotClient = new RiotClient(mockHttpClient.Object);
+            _output = output;
         }
 
         [Fact]
-        public async Task GetAccountTestAsync()
+        public async Task GetAccountTest()
         {
             string username = "sofieee";
             string region = "euw1";
@@ -23,6 +30,17 @@ namespace OvStats_Website_Test
 
             Assert.NotNull(summonerAccountDTO.id);
             Assert.Equal("sofieee", summonerAccountDTO.name);
+        }
+
+        [Fact]
+        public async Task GetSummonerInfoTest()
+        {
+            string userId = "EKuybWPs3S3E2seF6dk2Qj7LYpwPCpi-kFWiJek-Shhc86H9";
+            string region = "euw1";
+            IEnumerable<SummonerStatsDTO> summonerStats = await _IRiotClient.GetSummonerInfo(userId, region);
+        
+            Assert.NotNull(summonerStats);
+            Assert.Contains(summonerStats, stats => stats.queueType == "RANKED_SOLO_5x5");
         }
     }
 }
